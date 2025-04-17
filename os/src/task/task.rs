6d06cbccc82d1ddc2b84e1993e5e19b2cfff6db7
X.P <a1679190@gmail.com>
2025-04-17 10:17:51 +0800

    spawn系统调用

--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -231,6 +231,52 @@ impl TaskControlBlock {
         // ---- release parent PCB
     }
 
+    /// fn spawn
+    pub fn spawn(self: &Arc<Self>, elf_data: &[u8]) -> Option<Arc<Self>> {
+        let (memory_set, user_sp, entry_point) = MemorySet::from_elf(elf_data);
+        let trap_cx_ppn = memory_set
+            .translate(VirtAddr::from(TRAP_CONTEXT_BASE).into())
+            .unwrap()
+            .ppn();
+
+        let pid_handle = pid_alloc();
+        let kernel_stack = kstack_alloc();
+        let kernel_stack_top = kernel_stack.get_top();
+
+        let task_control_block = Arc::new(TaskControlBlock {
+            pid: pid_handle,
+            kernel_stack,
+            inner: unsafe {
+                UPSafeCell::new(TaskControlBlockInner {
+                    trap_cx_ppn,
+                    base_size: user_sp,
+                    task_cx: TaskContext::goto_trap_return(kernel_stack_top),
+                    task_status: TaskStatus::Ready,
+                    memory_set,
+                    parent: Some(Arc::downgrade(self)),
+                    children: Vec::new(),
+                    exit_code: 0,
+                    heap_bottom: user_sp,
+                    program_brk: user_sp,
+                })
+            },
+        });
+
+        let mut inner = self.inner_exclusive_access();
+        inner.children.push(task_control_block.clone());
+
+        let trap_cx = task_control_block.inner_exclusive_access().get_trap_cx();
+        *trap_cx = TrapContext::app_init_context(
+            entry_point,
+            user_sp,
+            KERNEL_SPACE.exclusive_access().token(),
+            kernel_stack_top,
+            trap_handler as usize,
+        );
+
+        Some(task_control_block)
+    }
+
     /// get pid of process
     pub fn getpid(&self) -> usize {
         self.pid.0
