1b94225722e114a6a310b1273f5ba873488c9654
X.P <a1679190@gmail.com>
2025-04-22 15:06:22 +0800

    合并 sys_get_time 添加空文档

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,10 +1,12 @@
+use core::mem::size_of;
+
 use crate::{
     fs::{open_file, OpenFlags},
-    mm::{translated_ref, translated_refmut, translated_str},
+    mm::{translated_byte_buffer, translated_ref, translated_refmut, translated_str},
     task::{
         current_process, current_task, current_user_token, exit_current_and_run_next, pid2process,
         suspend_current_and_run_next, SignalFlags,
-    },
+    }, timer::get_time_us,
 };
 use alloc::{string::String, sync::Arc, vec::Vec};
 
@@ -151,12 +153,29 @@ pub fn sys_kill(pid: usize, signal: u32) -> isize {
 /// YOUR JOB: get time with second and microsecond
 /// HINT: You might reimplement it with virtual memory management.
 /// HINT: What if [`TimeVal`] is splitted by two pages ?
-pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
-    trace!(
-        "kernel:pid[{}] sys_get_time NOT IMPLEMENTED",
-        current_task().unwrap().process.upgrade().unwrap().getpid()
-    );
-    -1
+pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
+    let us = get_time_us();
+    let time_val = TimeVal {
+        sec: us / 1_000_000,
+        usec: us % 1_000_000,
+    };
+
+    // 将结构体转换为字节切片
+    let time_val_bytes = unsafe {
+        core::slice::from_raw_parts(
+            (&time_val as *const TimeVal) as *const u8,
+            size_of::<TimeVal>(),
+        )
+    };
+    let token = current_user_token();
+    let data_ptr = ts as *mut u8;
+    // 简单地按字节逐个拷贝，效率较低
+    let v = translated_byte_buffer(token, data_ptr, size_of::<TimeVal>());
+    let flat_dest_iter = v.into_iter().flatten();
+    for (dest_byte, src_byte) in flat_dest_iter.zip(time_val_bytes.iter()) {
+        *dest_byte = *src_byte;
+    }
+    0
 }
 
 /// mmap syscall
